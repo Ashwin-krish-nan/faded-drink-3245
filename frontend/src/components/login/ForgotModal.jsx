9fe477da4d853489450764c213733ab3507c79d2
himanshuabd17 <himanshu12061998@gmail.com>
2023-05-06 20:52:31 +0530

    login

---
+++ frontend/src/components/login/ForgotModal.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+// import HOST from "../../utils/baseUrl";
+
+const ForgotModal = () => {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const showModal = () => {
+    setOpen(true);
+    console.log(email);
+  };
+
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 3000);
+  };
+
+  // let forgetPassword = async (data) => {
+  //   try {
+  //     let json = await fetch(`${HOST}/user/forgotpassword}`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(data),
+  //     });
+  //     let response = await json.json();
+  //     console.log(response);
+  //     localStorage.setItem("userID", response.data.userId);
+  //     if (response.Status === "Success") {
+  //       alert(response.msg);
+  //     } else {
+  //       alert("Can't Find user linked to this email");
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button className="forgotpassbtn" onClick={showModal}>
+        Forgot Password?
+      </Button>
+      <Modal
+        open={open}
+        title="Reset Password with Email"
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Return
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            loading={loading}
+            onClick={handleOk}
+          >
+            Submit
+          </Button>,
+        ]}
+      >
+        <p>Email</p>
+        <input
+          className="InputModalt"
+          type="text"
+          placeholder="Enter Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Modal>
+    </>
+  );
+};
+
+export default ForgotModal;
